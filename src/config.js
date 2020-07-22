0997bc14147a80ea82bd214562c985b8274e82b0
J. Patrick <jpatricknola@gmail.com>
2020-07-22 09:51:26 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "serverless-engineering-meeting"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://iyk38x22kk.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_4p5ClApHG",
+    APP_CLIENT_ID: "3pgfiorkugidnpaqllk0hdpuep",
+    IDENTITY_POOL_ID: "us-east-1:68bd6bd4-2192-482a-a928-807709fc32cb"
+  }
+};
